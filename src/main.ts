56ca5e4ed1eca8f94eefa829b3d56733221e7816
thefoku <leekengham@gmail.com>
2025-06-10 15:42:36 +0400

    feat: add PrimeVue theme settings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import { MainPreset } from '@/services/theme/preset'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +12,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MainPreset,
+    options: {
+      prefix: 'p',
+    },
+  },
+})
 
 app.mount('#app')
